a20ec9f07881605a251546127260174b6d93f1b5
Mario Ruano <mrf1989@hotmail.com>
2016-11-14 19:04:55 +0100

    Composición de plantilla de envío del formulario de contacto

--- formulario.php
+++ formulario.php
@@ -4,13 +4,14 @@ if ($_POST["nombre"] && $_POST["email"] != "") {
     $emailRemite = $_POST["email"];
     $destino = "mr@marioruano.com";
     $asunto = $_POST["asunto"];
-    $mensaje .= "De: " . $remite . "\n" . $_POST["mensaje"];
+    $mensaje .= "Enviado por: " . $remite . "\n" . "Email: " . $emailRemite . "\n\n" . "Asunto: " . $asunto . "\n" . $_POST["mensaje"];
     $emailheader = "From: Contacto portafolio <mr@marioruano.com>\r\n";
     mail($destino, $asunto, $mensaje, $emailheader) or die ("Lo siento, su mensaje no ha podido ser enviado.<br />Inténtelo de nuevo, por favor.");
     echo utf8_decode(utf8_decode("Su mensaje ha sido enviado correctamente."));
 } else {
     if ($_POST["nombre"] == "" || $_POST["email"] == "" || $_POST["mensaje"] == "") {
         echo utf8_decode("Todos los campos de texto son obligatorios");
+        exit;
     }
 }
 ?>
