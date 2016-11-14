29a7ff95c92b8790649bb47dafd34d720de71b69
Mario Ruano <mrf1989@hotmail.com>
2016-11-14 19:09:45 +0100

    Corrección de remite

--- formulario.php
+++ formulario.php
@@ -5,7 +5,7 @@ if ($_POST["nombre"] && $_POST["email"] != "") {
     $destino = "mr@marioruano.com";
     $asunto = $_POST["asunto"];
     $mensaje .= "Enviado por: " . $remite . "\n" . "Email: " . $emailRemite . "\n\n" . "Asunto: " . $asunto . "\n" . $_POST["mensaje"];
-    $emailheader = "From: Contacto portafolio <mr@marioruano.com>\r\n";
+    $emailheader = "From: " . $remite . "<" . $emailRemite . ">\r\n";
     mail($destino, $asunto, $mensaje, $emailheader) or die ("Lo siento, su mensaje no ha podido ser enviado.<br />Inténtelo de nuevo, por favor.");
     echo utf8_decode(utf8_decode("Su mensaje ha sido enviado correctamente."));
 } else {
