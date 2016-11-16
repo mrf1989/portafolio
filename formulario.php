adeb5b881d2c3cf1205626076a033ec7aa4b8c7e
Mario Ruano <mrf1989@hotmail.com>
2016-11-16 11:06:51 +0100

    Jump links solucionados y visualizados en el viewport

--- formulario.php
+++ formulario.php
@@ -4,13 +4,13 @@ if ($_POST["nombre"] && $_POST["email"] != "") {
     $emailRemite = $_POST["email"];
     $destino = "mr@marioruano.com";
     $asunto = $_POST["asunto"];
-    $mensaje .= "Enviado por: " . $remite . "\n" . "Email: " . $emailRemite . "\n\n" . "Asunto: " . $asunto . "\n" . $_POST["mensaje"];
+    $mensaje .= "Enviado por: " . utf8_decode($remite) . "\n" . "Email: " . utf8_decode($emailRemite) . "\n\n" . "Asunto: " . utf8_decode($asunto) . "\n" . utf8_decode($_POST["mensaje"]);
     $emailheader = "From: " . $remite . "<" . $emailRemite . ">\r\n";
     mail($destino, $asunto, $mensaje, $emailheader) or die ("Lo siento, su mensaje no ha podido ser enviado.<br />Inténtelo de nuevo, por favor.");
     echo utf8_decode(utf8_decode("Su mensaje ha sido enviado correctamente."));
 } else {
     if ($_POST["nombre"] == "" || $_POST["email"] == "" || $_POST["mensaje"] == "") {
-        echo utf8_decode("Todos los campos de texto son obligatorios\n" . "<a href='index.html'>Regresar al portafolio</a>");
+        echo utf8_decode("Todos los campos de texto son obligatorios\n");
         exit;
     }
 }
